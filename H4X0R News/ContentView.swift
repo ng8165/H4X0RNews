59a60cdd662966a22e57732fde64987aac3df0f3
Nelson Gou <nel4gou@gmail.com>
2022-07-01 16:06:55 -0700

    Finish app design

--- H4X0R News/ContentView.swift
+++ H4X0R News/ContentView.swift
@@ -9,10 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(posts, rowContent: { post in
+                Text(post.title)
+            })
+            .listStyle(.inset)
+            
+            .navigationTitle("H4X0R News")
+        }
     }
 }
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "hello"),
+    Post(id: "2", title: "world"),
+    Post(id: "3", title: "my"),
+    Post(id: "4", title: "name"),
+    Post(id: "5", title: "is"),
+    Post(id: "6", title: "nelson")
+]
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
