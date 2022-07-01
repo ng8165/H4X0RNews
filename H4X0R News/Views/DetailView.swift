6927ceed1c4738a8f5d943e50547c7a328124927
Nelson Gou <nel4gou@gmail.com>
2022-07-01 16:56:05 -0700

    Add WebKit view for each article

---
+++ H4X0R News/Views/DetailView.swift
@@ -0,0 +1,22 @@
+//
+//  DetailView.swift
+//  H4X0R News
+//
+//  Created by Nelson Gou on 7/1/22.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://nelsongou.me")
+    }
+}
---
+++ H4X0R News/Views/WebView.swift
@@ -0,0 +1,26 @@
+//
+//  WebView.swift
+//  H4X0R News
+//
+//  Created by Nelson Gou on 7/1/22.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let urlString = urlString, let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            uiView.load(request)
+        }
+    }
+    
+    typealias UIViewType = WKWebView
+}
